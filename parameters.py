9b241c2e0a930d72e4d674f90068099c0c1971ac
Sandeep Singh Negi (RBEI/BSV-AD1) <Negi.SandeepSingh@in.bosch.com>
2021-04-09 23:35:42 +0530

    just changed the id and password and keywords

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 file_name = 'connections.csv'
-linkedin_username = 'negi.sandeep01906@gmail.com'
-linkedin_password = 'Sunder!@#L@xmi967'
+linkedin_username = 'yourEmailAdress@gmail.com'
+linkedin_password = 'yourPassword'
 keywords = 'Technical Recruiter,IT hiring,Technical Recruiter,CTO,IT Recruitement'
 till_page = 10
