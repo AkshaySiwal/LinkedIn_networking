a77345292f0bed13efc23f4fc87bc2889d86ce47
Akshay Siwal <akshay.singh.siwal@gmail.com>
2023-02-12 11:26:18 +0530

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -2,4 +2,5 @@ file_name = 'connections.csv'
 linkedin_username = 'XXXXX@gmail.com'
 linkedin_password = 'XXXXXX'
 keywords = 'DevOps,Linux,Python,AWS'
+ignore_list = 'Person1,Person2'
 till_page = 10
